bb07339fd63296d596dee36a9f76c7aa55e8c481
MrCsabaToth <csaba.toth.us@gmail.com>
2022-10-06 17:14:57 -0700

    Adjust badge test for our data

--- src/models/badge.test.ts
+++ src/models/badge.test.ts
@@ -4,9 +4,9 @@ import { allKeys } from './utils';
 
 describe('badge', () => {
   it('matches the shape of the default data', () => {
-    const badges: Badge[] = Object.values(data['speakers']['dmytro_danylyk']['badges']);
+    const badges: Badge[] = Object.values(data['speakers']['mark_johnson']['badges']);
     const keys: Array<keyof Badge> = ['description', 'link', 'name'];
-    expect(badges).toHaveLength(2);
+    expect(badges).toHaveLength(4);
     expect(allKeys(badges)).toStrictEqual(keys);
   });
 });
