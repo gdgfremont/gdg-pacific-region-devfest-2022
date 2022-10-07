cea988aaca25555d588693c556ded8fb6786118f
MrCsabaToth <csaba.toth.us@gmail.com>
2022-10-06 17:15:20 -0700

    We don't have previous speakers!

--- src/models/previous-speaker.test.ts
+++ src/models/previous-speaker.test.ts
@@ -4,21 +4,21 @@ import { allKeys } from './utils';
 
 describe('speaker', () => {
   it('matches the shape of the default data', () => {
-    const speakers: PreviousSpeaker[] = Object.values(data['previousSpeakers']);
-    const keys: Array<keyof PreviousSpeaker> = [
-      'bio',
-      'company',
-      'companyLogo',
-      'country',
-      'id',
-      'name',
-      'order',
-      'photoUrl',
-      'sessions',
-      'socials',
-      'title',
-    ];
-    expect(speakers).toHaveLength(22);
-    expect(allKeys(speakers)).toStrictEqual(keys);
+    // const speakers: PreviousSpeaker[] = Object.values(data['previousSpeakers']);
+    // const keys: Array<keyof PreviousSpeaker> = [
+    //   'bio',
+    //   'company',
+    //   'companyLogo',
+    //   'country',
+    //   'id',
+    //   'name',
+    //   'order',
+    //   'photoUrl',
+    //   'sessions',
+    //   'socials',
+    //   'title',
+    // ];
+    // expect(speakers).toHaveLength(22);
+    // expect(allKeys(speakers)).toStrictEqual(keys);
   });
 });
