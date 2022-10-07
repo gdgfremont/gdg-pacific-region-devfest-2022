7ea6e1a41bcab73f436e856af6e6369007646d3f
MrCsabaToth <csaba.toth.us@gmail.com>
2022-10-06 17:15:08 -0700

    We dont' have previous sessions!

--- src/models/previous-session.test.ts
+++ src/models/previous-session.test.ts
@@ -4,11 +4,11 @@ import { allKeys } from './utils';
 
 describe('speaker', () => {
   it('matches the shape of the default data', () => {
-    const sessions: PreviousSession[] = Object.values(
-      data['previousSpeakers']['adrian_kajda']['sessions']['2016']
-    );
-    const keys: Array<keyof PreviousSession> = ['presentation', 'tags', 'title', 'videoId'];
-    expect(sessions).toHaveLength(1);
-    expect(allKeys(sessions)).toStrictEqual(keys);
+    // const sessions: PreviousSession[] = Object.values(
+    //   data['previousSpeakers']['adrian_kajda']['sessions']['2016']
+    // );
+    // const keys: Array<keyof PreviousSession> = ['presentation', 'tags', 'title', 'videoId'];
+    // expect(sessions).toHaveLength(1);
+    // expect(allKeys(sessions)).toStrictEqual(keys);
   });
 });
