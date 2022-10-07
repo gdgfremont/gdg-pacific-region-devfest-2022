758b0a7f79a0fcd4dc7e9800ecb798d87318d065
MrCsabaToth <csaba.toth.us@gmail.com>
2022-10-06 17:16:12 -0700

    Adjust time slots test to our data

--- src/models/timeslot.test.ts
+++ src/models/timeslot.test.ts
@@ -4,9 +4,9 @@ import { allKeys } from './utils';
 
 describe('timeslot', () => {
   it('matches the shape of the default data', () => {
-    const days: Timeslot[] = data['schedule']['2016-09-09']['timeslots'];
+    const days: Timeslot[] = data['schedule']['2022-11-03']['timeslots'];
     const keys: Array<keyof Timeslot> = ['endTime', 'sessions', 'startTime'];
-    expect(days).toHaveLength(13);
+    expect(days).toHaveLength(16);
     expect(allKeys(days)).toStrictEqual(keys);
   });
 });
