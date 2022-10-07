34bd45d9dcee2a244a0099e3a3d7afb2aa14674c
MrCsabaToth <csaba.toth.us@gmail.com>
2022-10-06 17:16:22 -0700

    Adjust track test to our data

--- src/models/track.test.ts
+++ src/models/track.test.ts
@@ -4,9 +4,9 @@ import { allKeys } from './utils';
 
 describe('track', () => {
   it('matches the shape of the default data', () => {
-    const days: Track[] = data['schedule']['2016-09-09']['tracks'];
+    const days: Track[] = data['schedule']['2022-11-03']['tracks'];
     const keys: Array<keyof Track> = ['title'];
-    expect(days).toHaveLength(3);
+    expect(days).toHaveLength(2);
     expect(allKeys(days)).toStrictEqual(keys);
   });
 });
